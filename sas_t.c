aef29dd6444b6ea76942b9cfae8a4cb5da38d304
safae-ycode <safaeerray@gmail.com>
2025-09-02 13:05:01 +0200

    chapitre 3

--- sas_t.c
+++ sas_t.c
@@ -1,1 +1,57 @@
-le 2émé changement
+//le 1er changement
+#include <stdio.h>
+int main(void)
+{
+printf("hello, world\n");
+return 0;
+}
+//le 2eme 
+#include <stdio.h>
+int main(void)
+{
+printf("Parkinson's Law:\nWork expands so as to ");
+printf("fill the time\n");
+printf("available for its completion.\n");
+return 0;
+}
+//(a)la fonction printf provient de la biblio stdio.h 
+//(b)la sortie du programme est  
+/*Parkinson's Law
+Work expands so as to fill the time
+available for its completion.*/
+
+//le 3eme changment 
+int main(void) {
+int height, length, width, volume, weight;
+//height => heig  || length=>len || volume =>vol || weight=>weig
+height = 8;
+length = 12;
+width = 10;
+volume = height * length * width;
+weight = (volume + 165) / 166;
+printf("Dimensions: %dx%dx%d\n", length, width, height);
+printf("Volume (cubic inches): %d\n", volume);
+printf("Dimensional weight (pounds): %d\n", weight);
+//printf("Dimensional weight (pounds): %d\n", (volume + 165) / 166 );
+
+return 0;
+}
+//le 4eme changement 
+int main(void){
+//    int var1,var2,var3;
+    printf("les variables sans initialisations et non  attribués sont ,%d,%d,%d")
+}
+
+//l'identifiant incorrectes est :
+(b) _100_bottles
+//un trait remplace un espacequi est interdit en nommage des variables est ameliore la lisibite 
+ 
+//les mots vles de C sont for if ,printf ,while 
+//le nombre de jeton est 18
+answer=(3*q-p*p)/3;
+//amelioration 
+answer=(3 * q - p * p) / 3
+
+
+printf("Dimensions: %dx%dx%d\n", length, width, height);
+//devient printf("Dimensions: %d x %d x %d \n", length, width, height);
